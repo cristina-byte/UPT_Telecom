f8d6ead43f3a0a00e8c417a4e470dafc79c8ea27
nadiagheorghiu <57364589+nadiagheorghiu@users.noreply.github.com>
2021-11-06 17:01:53 +0200

    Routes were created

--- Project/routes/web.php
+++ Project/routes/web.php
@@ -16,55 +16,63 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {return view('welcome');});
 
  
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\MainController::class, 'index'])->name('home');
 
-Route::get('/dashboard',[App\Http\Controllers\HomeController::class,'index'])->name('dashboard');
-Route::get('/clienti', [App\Http\Controllers\ADMIN\ClientController::class, 'index'])->name('clienti');
-Route::get('/angajati', [App\Http\Controllers\ADMIN\AngajatiController::class, 'index'])->name('angajati');
-Route::get('/departamente', [App\Http\Controllers\ADMIN\DepartamenteController::class, 'index'])->name('departamente');
-Route::get('/facturi', [App\Http\Controllers\ADMIN\FacturiController::class, 'index'])->name('facturi');
-Route::get('/servicii', [App\Http\Controllers\ADMIN\ServiciiController::class, 'index'])->name('servicii');
-Route::get('/tickete', [App\Http\Controllers\ADMIN\TicketeController::class, 'index'])->name('tickete');
+Route::get('/dashboard',[App\Http\Controllers\MainController::class,'index'])->name('dashboard');
+Route::get('/client', [App\Http\Controllers\ClientController::class, 'index'])->name('client');
+Route::get('/angajat', [App\Http\Controllers\AngajatController::class, 'index'])->name('angajat');
+Route::get('/departament', [App\Http\Controllers\DepartamentController::class, 'index'])->name('departament');
+Route::get('/factura', [App\Http\Controllers\FacturaController::class, 'index'])->name('factura');
+Route::get('/serviciu', [App\Http\Controllers\ServiciuController::class, 'index'])->name('serviciu');
+Route::get('/tichet', [App\Http\Controllers\TichetController::class, 'index'])->name('tichet');
  
 
-//clienti  -??
-Route::get('/clienti/{id}/new',[App\Http\Controllers\ADMIN\ClientiController::class,'create'])->name('clienti_create');
-Route::post('/clienti{id}/store',[App\Http\Controllers\ADMIN\ClientiController::class,'store'])->name('clienti.send');
-Route::get('/clienti/{id}',[App\Http\Controllers\ADMIN\ClientiController::class,'show'])->name('clienti.show');
-Route::post('/clienti/{id}/update',[App\Http\Controllers\ADMIN\ClientiPageController::class,'update'])->name('clienti.update');
-Route::get('/clienti/{id}/edit',[App\Http\Controllers\ADMIN\ClientiPageController::class,'edit'])->name('clienti.edit');
-
-
-Route::get('/delete_clienti/{id}',[App\Http\Controllers\ADMIN\ClientiController::class,'destroy'])->name('delete_clienti');
+//clienti  
+Route::get('/add_client',[App\Http\Controllers\ClientController::class,'create'])->name('add_client');
+Route::post('/store_client',[App\Http\Controllers\ClientController::class,'store'])->name('store.client');
+//Route::get('/client/{id}',[App\Http\Controllers\ClientController::class,'show'])->name('client.show');
+Route::post('/client/{id}/update',[App\Http\Controllers\ClientController::class,'update'])->name('client.update');
+Route::get('/client/{id}/edit',[App\Http\Controllers\ClientController::class,'edit'])->name('client.edit');
+Route::get('/delete_client/{id}',[App\Http\Controllers\ClientController::class,'destroy'])->name('delete.client');
 
 
 //angajat
-Route::get('/add_angajati',[App\Http\Controllers\ADMIN\AngajatiController::class,'create'])->name('add_angajati');
-Route::post('/store_angajati',[App\Http\Controllers\ADMIN\AngajatController::class,'store'])->name('store.angajati');
-Route::get('/edit_angajati/{id}',[App\Http\Controllers\ADMIN\AngajatController::class,'edit'])->name('edit_angajati');
-Route::post('/update_angajati',[App\Http\Controllers\ADMIN\AngajatiPageController::class,'update'])->name('update.angajati');
-Route::get('/delete_angajat/{id}',[App\Http\Controllers\ADMIN\AngajatController::class,'destroy'])->name('delete_angajati');
+Route::get('/add_angajat',[App\Http\Controllers\AngajatController::class,'create'])->name('add_angajat');
+Route::post('/store_angajat',[App\Http\Controllers\AngajatController::class,'store'])->name('store.angajat');
+Route::get('/angajat/{id}/edit',[App\Http\Controllers\AngajatController::class,'edit'])->name('edit_angajat');
+Route::post('/angajat{id}/update',[App\Http\Controllers\AngajatController::class,'update'])->name('update.angajat');
+Route::get('/delete_angajat/{id}',[App\Http\Controllers\AngajatController::class,'destroy'])->name('delete.angajat');
+//Route::get('/angajat/{id}',[App\Http\Controllers\AngajatController::class,'show'])->name('angajat.show');
 
 
-//tickete
-Route::get('/add_tickets',[App\Http\Controllers\ADMIN\TicketeController::class,'create'])->name('tickete');
-Route::post('/store_tickete',[App\Http\Controllers\ADMIN\TicketeController::class,'store'])->name('store.tickete');
-Route::get('/edit_tickete/{id}',[App\Http\Controllers\ADMIN\TicketeController::class,'edit'])->name('edit_tickete');
-Route::post('/update/{id}',[App\Http\Controllers\ADMIN\TicketeController::class,'update'])->name('update_tickete');
-Route::get('/delete_tickete/{id}',[App\Http\Controllers\ADMIN\TicketetController::class,'destroy'])->name('delete_tickete');
+//tichete
+Route::get('/add_tichet',[App\Http\Controllers\TichetController::class,'create'])->name('tichete');
+Route::post('/store_tichete',[App\Http\Controllers\TichetController::class,'store'])->name('store.tichete');
+Route::get('/tichet/{id}/edit',[App\Http\Controllers\TichetController::class,'edit'])->name('edit_tichete');
+Route::post('/tichet/{id}/update',[App\Http\Controllers\TichetController::class,'update'])->name('update_tichete');
+//Route::get('/delete_tichete/{id}',[App\Http\Controllers\TichetController::class,'destroy'])->name('delete_tichete');
+//Route::get('/tichet/{id}',[App\Http\Controllers\TichetController::class,'show'])->name('tichet.show');
 
 
 //servicii
-Route::get('/add_serviciu',[App\Http\Controllers\ADMIN\ServiciuController::class,'create'])->name('serviciu');
-Route::post('/store_serviciu',[App\Http\Controllers\ADMIN\ServiciuController::class,'store'])->name('store.serviciu');
-Route::get('/edit_serviciu/{id}',[App\Http\Controllers\ADMIN\ServiciuController::class,'edit'])->name('edit_serviciu');
-Route::post('/update/{id}',[App\Http\Controllers\ADMIN\ServiciuController::class,'update'])->name('update_serviciu');
-Route::get('/delete_serviciu/{id}',[App\Http\Controllers\ADMIN\ServiciuController::class,'destroy'])->name('delete_serviciu');
+Route::get('/add_serviciu',[App\Http\Controllers\ServiciuController::class,'create'])->name('serviciu');
+Route::post('/store_serviciu',[App\Http\Controllers\ServiciuController::class,'store'])->name('store.serviciu');
+Route::get('/serviciu/{id}/edit',[App\Http\Controllers\ServiciuController::class,'edit'])->name('edit_serviciu');
+Route::post('/serviciu/{id}/update',[App\Http\Controllers\ServiciuController::class,'update'])->name('update_serviciu');
+Route::get('/delete_serviciu/{id}',[App\Http\Controllers\ServiciuController::class,'destroy'])->name('delete_serviciu');
 
 
- 
-//Messages
-Route::get('messages',[App\Http\Controllers\ADMIN\EmailController::class,'index'])->name('messages');
-Route::get('/open/{id}',[App\Http\Controllers\ADMIN\EmailController::class,'show'])->name('open_message');
-Route::get('/delete/{id}',[App\Http\Controllers\ADMIN\EmailController::class,'destroy'])->name('delete_message');
-Route::delete('/delete_messages',[App\Http\Controllers\ADMIN\EmailController::class,'deleteSelected'])->name('delete_selected_messages');
+//factura
+Route::get('/add_factura',[App\Http\Controllers\FacturaController::class,'create'])->name('factura');
+Route::post('/store_factura',[App\Http\Controllers\FacturaController::class,'store'])->name('store.factura');
+Route::get('/factura/{id}/edit',[App\Http\Controllers\FacturaController::class,'edit'])->name('edit_factura');
+Route::post('/factura/{id}/update',[App\Http\Controllers\FacturaController::class,'update'])->name('update_factura');
+  
+
+//departament
+Route::get('/add_departament',[App\Http\Controllers\DepartamentController::class,'create'])->name('departament');
+Route::post('/store_departament',[App\Http\Controllers\DepartamentController::class,'store'])->name('store.departament');
+Route::get('/departament/{id}/edit',[App\Http\Controllers\DepartamentController::class,'edit'])->name('edit_departament');
+Route::post('/departament/{id}/update',[App\Http\Controllers\DepartamentController::class,'update'])->name('update_departament');
+Route::get('/delete_departament/{id}',[App\Http\Controllers\DepartamentController::class,'destroy'])->name('delete.departament');
+//Route::get('/departament/{id}',[App\Http\Controllers\DepartamentController::class,'show'])->name('departament.show');
