daa71077dbc53591ed95e61162cd538ae3af88a4
undcybele <andreea1bolea@gmail.com>
2021-12-11 15:13:12 +0200

    Added facturi neplatite pentru clienti function

--- Project/app/Http/Controllers/ClientController.php
+++ Project/app/Http/Controllers/ClientController.php
@@ -7,6 +7,7 @@ use App\Models\Client;
 use App\Models\Serviciu;
 
 use App\Http\Requests\StoreClientRequest;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 
 class ClientController extends Controller
@@ -14,64 +15,46 @@ class ClientController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-
-
         $clienti = Client::All();
         return view('clienti', ['clienti' => $clienti]);
     }
 
 
-  public function search(Request $request)
+    public function search(Request $request)
     {
-
-       $this->validate($request,['nume'=>'required']);
-
-      $clienti=Client::select('*')->where('nume',$request->nume)->get();
-
-
-
+        $this->validate($request, ['nume' => 'required']);
+        $clienti = Client::select('*')->where('nume', $request->nume)->get();
         return view('clienti', ['clienti' => $clienti]);
-
-
-
-
-
     }
 
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
-        $servicii=Serviciu::All();
-        return view('adaugare_client',['servicii'=>$servicii]);
+        $servicii = Serviciu::All();
+        return view('adaugare_client', ['servicii' => $servicii]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(StoreClientRequest $request)
     {
-
-
-
-
         $client = new Client;
-
         $client->nume = $request->nume;
         $client->prenume = $request->prenume;
-
-        $serviciu=Serviciu::select('servicii.id')->where('tip',$request->pachet)->get();
+        $serviciu = Serviciu::select('servicii.id')->where('tip', $request->pachet)->get();
         $client->id_serviciu = $serviciu[0]->id;
         $client->email = $request->email;
         $client->adresa = $request->adresa;
@@ -80,15 +63,14 @@ class ClientController extends Controller
         $client->durata_min = $request->durata;
 
         $client->save();
-
         return redirect()->route('client');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function show($id)
     {
@@ -98,29 +80,25 @@ class ClientController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function edit($id)
     {
-        $client=Client::findOrFail($id);
-        return view("editare_client",['client'=>$client]);
+        $client = Client::findOrFail($id);
+        return view("editare_client", ['client' => $client]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param int $id
+     * @return Response
      */
     public function update(StoreClientRequest $request, $id)
     {
-
-
-
         $client = Client::findOrFail($id);
-
         $client->nume = $request->nume;
         $client->prenume = $request->prenume;
         $client->id_serviciu = $request->pachet;
@@ -131,15 +109,14 @@ class ClientController extends Controller
         $client->durata_min = $request->durata;
 
         $client->save();
-
         return redirect()->route('client');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function destroy($id)
     {
@@ -149,7 +126,7 @@ class ClientController extends Controller
 
     public function neplatit()
     {
-       $clienti = DB::select(DB::raw("SELECT * FROM facturi LEFT JOIN clienti on facturi.id_client = clienti.id WHERE facturi.status = 'neplatit'"));
-       dd($clienti);
+        $clienti = DB::select(DB::raw("SELECT * FROM facturi LEFT JOIN clienti on facturi.id_client = clienti.id WHERE facturi.status = 'neplatit'"));
+        return view('clienti', ['clienti' => $clienti]);
     }
 }
